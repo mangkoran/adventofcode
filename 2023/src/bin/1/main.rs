f5063d6e1c5abdd3a50e4af8e135bec8e93c176c
Afiq Nazrie <afnazrie@gmail.com>
2023-12-03 13:34:51 +0700

    refactor: 23 day1

--- 2023/src/bin/1/main.rs
+++ 2023/src/bin/1/main.rs
@@ -1,105 +1,13 @@
+// https://adventofcode.com/2023/day/1
 use mgk_aoc::*;
 use std::collections::HashMap;
 use std::path::Path;
 
 fn main() {
-    println!("{}", part_one("./input.txt"));
-    println!("{}", part_two("./input.txt"));
-}
-
-fn get_calibration_value(text: &str, part_two: bool) -> u32 {
-    let mut tenth: u32 = 0;
-    let mut one: u32 = 0;
-    let mut tenth_index: u32 = 0;
-    let mut one_index: u32 = 0;
-
-    let text_to_digit = HashMap::from([
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ]);
-
-    if part_two {
-        for (key, value) in text_to_digit.into_iter() {
-            // tenth_index = u32::try_from(text.find(key).unwrap()).unwrap();
-            if let Some(i) = text.find(key) {
-                if let Ok(y) = u32::try_from(i) {
-                    // println!("{y} - {key} - {value} - {tenth_index} - {one_index}");
-                    if tenth == 0 || y < tenth_index {
-                        tenth_index = y;
-                        tenth = value;
-                    }
-
-                    if y > one_index {
-                        one_index = y;
-                        one = value;
-                    }
-                }
-            }
-
-            if let Some(i) = text.rfind(key) {
-                if let Ok(y) = u32::try_from(i) {
-                    // println!("{y} - {key} - {value} - {tenth_index} - {one_index}");
-                    if tenth == 0 || y < tenth_index {
-                        tenth_index = y;
-                        tenth = value;
-                    }
-
-                    if y > one_index {
-                        one_index = y;
-                        one = value;
-                    }
-                }
-            }
-        }
-    }
-
-    for c in text.chars() {
-        if c.is_ascii_digit() {
-            if let Some(i) = text.find(c) {
-                if let Ok(y) = u32::try_from(i) {
-                    // println!("{y} - {key} - {value} - {tenth_index} - {one_index}");
-                    if tenth == 0 || y < tenth_index {
-                        tenth_index = y;
-                        tenth = c.to_digit(10).unwrap()
-                    }
-
-                    if y > one_index {
-                        one_index = y;
-                        one = c.to_digit(10).unwrap()
-                    }
-                }
-            }
-
-            if let Some(i) = text.rfind(c) {
-                if let Ok(y) = u32::try_from(i) {
-                    // println!("{y} - {key} - {value} - {tenth_index} - {one_index}");
-                    if tenth == 0 || y < tenth_index {
-                        tenth_index = y;
-                        tenth = c.to_digit(10).unwrap()
-                    }
-
-                    if y > one_index {
-                        one_index = y;
-                        one = c.to_digit(10).unwrap()
-                    }
-                }
-            }
-        }
-    }
-
-    if one == 0 {
-        one = tenth
-    }
-
-    // println!("{tenth}{one}");
-    format!("{tenth}{one}").parse().unwrap()
+    // println!("Part one: {}", part_one("./input_example.txt"));
+    println!("Part one: {}", part_one("./input.txt"));
+    // println!("Part two: {}", part_two("./input_example.txt"));
+    println!("Part two: {}", part_two("./input.txt"));
 }
 
 fn part_one<P>(filename: P) -> u32
@@ -107,8 +15,6 @@ where
     P: AsRef<Path>,
 {
     let mut sum: u32 = 0;
-
-    // get_calibration_value(input);
 
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
@@ -127,17 +33,117 @@ where
 {
     let mut sum: u32 = 0;
 
-    // get_calibration_value(input);
-
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
-            if let Ok(input) = line {
-                sum += get_calibration_value(input.as_str(), true)
+            if let Ok(line) = line {
+                sum += get_calibration_value(line.as_str(), true)
             }
         }
     }
 
     sum
+}
+
+fn get_calibration_value(line: &str, part_two: bool) -> u32 {
+    let mut tenth: u32 = 0;
+    let mut tenth_index: u32 = 0;
+    let mut one: u32 = 0;
+    let mut one_index: u32 = 0;
+
+    let text_to_digit = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    if part_two {
+        for (text, digit) in text_to_digit.into_iter() {
+            if let Some(index) = line.find(text) {
+                swap_digit(
+                    &mut tenth,
+                    &mut tenth_index,
+                    &mut one,
+                    &mut one_index,
+                    index,
+                    digit,
+                )
+            }
+
+            if let Some(index) = line.rfind(text) {
+                swap_digit(
+                    &mut tenth,
+                    &mut tenth_index,
+                    &mut one,
+                    &mut one_index,
+                    index,
+                    digit,
+                )
+            }
+        }
+    }
+
+    for c in line.chars() {
+        if c.is_ascii_digit() {
+            let digit = c.to_digit(10).unwrap();
+
+            if let Some(index) = line.find(c) {
+                swap_digit(
+                    &mut tenth,
+                    &mut tenth_index,
+                    &mut one,
+                    &mut one_index,
+                    index,
+                    digit,
+                )
+            }
+
+            if let Some(index) = line.rfind(c) {
+                swap_digit(
+                    &mut tenth,
+                    &mut tenth_index,
+                    &mut one,
+                    &mut one_index,
+                    index,
+                    digit,
+                )
+            }
+        }
+    }
+
+    if one == 0 {
+        one = tenth
+    }
+
+    // println!("{tenth}{one}");
+    format!("{tenth}{one}").parse().unwrap()
+}
+
+fn swap_digit(
+    tenth: &mut u32,
+    tenth_index: &mut u32,
+    one: &mut u32,
+    one_index: &mut u32,
+    index: usize,
+    digit: u32,
+) {
+    if let Ok(y) = u32::try_from(index) {
+        // println!("{tenth_index} - {one_index}");
+        if *tenth == 0 || y < *tenth_index {
+            *tenth_index = y;
+            *tenth = digit
+        }
+
+        if y > *one_index {
+            *one_index = y;
+            *one = digit
+        }
+    }
 }
 
 // ref
